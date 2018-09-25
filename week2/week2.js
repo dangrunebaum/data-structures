8292e6621322342343e82b5d985a20240f22f863
EC2 Default User <ec2-user@ip-172-31-23-5.us-east-2.compute.internal>
2018-09-25 22:08:48 +0000

    adding materials for week

--- week2/week2.js
+++ week2/week2.js
@@ -2,20 +2,21 @@
 
 var fs = require('fs');
 var cheerio = require('cheerio');
-
+ var streetAddresses = [];
 // load the thesis text file into a variable, `content`
 // this is the file that we created in the starter code from last week
-var content = fs.readFileSync('m02.txt');
+var content = fs.readFileSync('../week2/m02.txt');
 
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
- var streetAddresses = [];  
+  
+ 
 // print table cell elements
 $('td').each(function(i, elem) {
    
 // limit by attribute "style" then remove the break element and trim white space     
     if($(elem).attr("style")=="border-bottom:1px solid #e3e3e3; width:260px"){
-    streetAddresses.push($(elem).html().split("<br>")[2].trim());
+    streetAddresses.push($(elem).html().split("<br>")[2].trim().split(',')[0]);
     //console.log(streetAddresses)
     
     }
@@ -23,5 +24,5 @@ $('td').each(function(i, elem) {
 
 
 });
-console.log(streetAddresses)
-fs.writeFileSync('./streetAddresses.txt', streetAddresses);
+//console.log(streetAddresses)
+fs.writeFileSync('streetAddresses.txt', JSON.stringify(streetAddresses));
